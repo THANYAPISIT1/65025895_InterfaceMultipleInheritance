2c7ba96e6d94aa9bd0aa49f9e7731b1f7c1366dd
THANYAPISIT1 <65025895@up.ac.th>
2023-07-18 20:44:10 +0700

    Create IMI.java

---
+++ IMI.java
@@ -0,0 +1,38 @@
+public class IMI {
+    public static void main(String[] args) {
+        Player p = new Player();
+        p.attack();
+        p.defend();
+        p.castSpell("Pisunk Pisunk rat ta na tha ya");
+        p.useweapon();
+    }
+}
+
+interface Character{
+    void attack();
+    void defend();
+}
+
+interface Mage{
+    void castSpell(String spell);
+}
+
+interface Warrior{
+    void useweapon();
+}
+
+class Player implements Character, Mage, Warrior{
+    
+    public void attack(){
+        System.out.println("The player attacks the enemy.");
+    }
+    public void defend(){
+        System.out.println("The player defends against the enemy's attack.");
+    }
+    public void castSpell(String spell){
+        System.out.println("The player casts "+spell);
+    }
+    public void useweapon(){
+        System.out.println("The player use a weapon to the enemy.");
+    }
+}
